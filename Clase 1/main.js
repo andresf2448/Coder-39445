5e9bd646f9dd3bf04e63b2b6c2f62f09b5575fdc
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-02-01 19:42:38 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,49 @@
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//Declaracion y asignación de una variable
+// let edad = 45; //forma recomendada
+// var nombre = "andres"; //forma no recomendada
+// const NUMERO = 10;
+
+// let nombre = "camila";
+// console.log("aca esta el nombre", nombre);
+
+// nombreBonitoGrande //camel case
+// nombre_bonito_grande //snake case
+// NombreBonitoGrande //pascal case
+
+// let numeroUno = 4;
+// let numeroDos = 5;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("resultado de la suma", resultadoSuma);
+// console.log("resultado de la resta", resultadoResta);
+// console.log("resultado de la multiplicacion", resultadoMultiplicacion);
+// console.log("resultado de la division", resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre =  "Nata";
+
+// let mensaje = saludo + " " + nombre;
+let mensaje = `esto es el saludo ${saludo} ${nombre}`;
+// let mensajeLargo = "esto es el saludo" + saludo + " " + nombre;
+
+// console.log(mensaje);
+// console.log("hola" + 1);
+// console.log(4+5);
+
+// let nombre = prompt("Ingrese su nombre");
+// alert(nombre);
+
+let edad = parseInt(prompt("Ingrese su edad"));
+console.log(typeof(edad));
