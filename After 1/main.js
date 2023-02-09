3cecc4be0ea6517e1afd7337c63a2912ec0a4153
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-02-09 16:46:54 -0500

    After 1

---
+++ After 1/main.js
@@ -0,0 +1,31 @@
+// let i = 1;
+
+// while(i < 3){
+//   console.log("Hola", i);
+//   i++;
+// }
+
+let usuario = prompt("Ingrese el usuario");
+
+while(usuario != "andres"){
+  alert("Usuario incorrecto");
+  usuario = prompt("Ingrese el usuario");
+}
+
+// for(let i = 1; i < 10; i++){
+//   if(i % 2 === 0){
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// let i = 1;
+
+// while(i < 10){
+//   if(i % 2 !== 0){
+//     console.log(i);
+//   }
+
+//   i++;
+// }
