abc7a048abf4e02da1970e3a9cd4641806319b78
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-03-08 19:30:44 -0500

    Clase 10

--- Clase 10/main.js
+++ Clase 10/main.js
@@ -23,3 +23,81 @@
 
 // localStorage.removeItem(clave); //elimina solo un elemento
 // localStorage.clear(); //elimina todo el storage
+
+// let objeto = {
+//   nombre: "andres",
+//   edad: 26,
+// };
+
+// localStorage.setItem("objeto", JSON.stringify(objeto));
+// localStorage.setItem("array", JSON.stringify([1, 2, 3, 4, 5]));
+
+// let objeto = JSON.parse(localStorage.getItem("objeto"));
+// console.log(objeto);
+
+// const productos = [
+//   { id: 1, nombre: "zapato", precio: 1000 },
+//   { id: 2, nombre: "camisa", precio: 1350 },
+//   { id: 3, nombre: "media", precio: 750 },
+//   { id: 4, nombre: "gorra", precio: 411 },
+// ];
+
+// const guardar = (clave, valor) => {
+//   localStorage.setItem(clave, valor);
+// };
+
+// productos.forEach(item => {
+//   guardar(item.id, JSON.stringify(item))
+// });
+
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+// let usuario;
+// let usuarioStorage = sessionStorage.getItem("usuario");
+
+// if(usuarioStorage){
+//   usuario = usuarioStorage;
+//   alert(`Bienvenido nuevamente ${usuario}`);
+// }else{
+//   usuario = prompt("Ingrese el usuario");
+//   alert(`Hola ${usuario} es tu primera vez`);
+//   sessionStorage.setItem("usuario", usuario);
+// }
+
+// const productos = [
+//   { id: 1, nombre: "zapato", precio: 1000 },
+//   { id: 2, nombre: "camisa", precio: 1350 },
+//   { id: 3, nombre: "media", precio: 750 },
+//   { id: 4, nombre: "gorra", precio: 411 },
+// ];
+
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+let boton = document.getElementById("borrar");
+let carrito = [];
+let carritoStorage = localStorage.getItem("carrito");
+
+if (carritoStorage) {
+  carrito = JSON.parse(carritoStorage);
+} else {
+  let div = document.createElement("div");
+  div.innerHTML = "El carrito está vacio";
+  document.body.append(div);
+}
+
+carrito.forEach((item) => {
+  let div = document.createElement("div");
+  div.innerHTML = `
+    <h2>Id: ${item.id}</h2>
+    <p>Nombre: ${item.nombre}</p>
+    <b>$${item.precio}</b>
+  `;
+
+  document.body.append(div);
+});
+
+boton.addEventListener("click", () => {
+  localStorage.clear();
+  alert("carrito borrado");
+  location.reload();
+});
